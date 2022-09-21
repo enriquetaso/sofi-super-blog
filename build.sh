a281e818094f61a7a54edda63864a7aa8fdc5b10
Sofia Enriquez <lsofia.enriquez@gmail.com>
2022-09-20 22:27:27 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,4 +9,4 @@ poetry install -vvv
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL --password $DJANGO_SUPERUSER_PASSWORD
+python manage.py createsuperuser --noinput
