08f62c4c93ac9c2207fb77f7a7b5c74f01683372
Sofia Enriquez <lsofia.enriquez@gmail.com>
2023-01-24 21:02:44 +0000

    WIP import csv command and tests

--- finance/tests.py
+++ finance/tests.py
@@ -23,3 +23,32 @@ class TestTransactionViewSet(TestCase):
 
     def test_permission_classes(self):
         self.assertEqual([IsAuthenticated], TransactionViewSet().get_permissions())
+
+
+import unittest
+from app.models import Transaction
+from app.serializers import TransactionSerializer
+
+
+class TestTransactionSerializer(unittest.TestCase):
+    def setUp(self):
+        self.transaction = Transaction.objects.create(
+            account="test_account",
+            date="2020-01-01",
+            description="test_description",
+            place="test_place",
+            amount=100,
+            tags="test_tags",
+            category="test_category",
+        )
+
+    def test_serializer(self):
+        serializer = TransactionSerializer(instance=self.transaction)
+
+        self.assertEqual(serializer.data["id"], self.transaction.id)
+        self.assertEqual(serializer.data["account"], "test_account")
+        self.assertEqual(serializer.data["date"], "2020-01-01")
+        self.assertEqual(serializer.data["description"], "test_description")
+        self.assertEqual(serializer.data["place"], "test_place")
+        self.assertEqual(serializer.data["amount"], 100)
+        self
