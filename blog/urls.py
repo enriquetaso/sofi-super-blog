6a01b3914d004b3d7ff301d8014d69726d26d00f
Sofia Enriquez <lsofia.enriquez@gmail.com>
2023-01-27 23:17:27 +0000

    More templates added

--- blog/urls.py
+++ blog/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.PostListView.as_view(), name="post_list"),
-    path("post/<int:pk>/", views.PostDetailView.as_view(), name="post_detail"),
-    path("post/new/", views.PostCreateFormView.as_view(), name="post_new"),
-    path("post/<int:pk>/edit/", views.post_edit, name="post_edit"),
+    path("", views.index, name="index"),
+    path("resume/", views.resume, name="resume"),
+    path("blog/", views.PostListView.as_view(), name="post_list"),
+    path("blog/post/<int:pk>/", views.PostDetailView.as_view(), name="post_detail"),
+    path("blog/post/new/", views.PostCreateFormView.as_view(), name="post_new"),
+    path("blog/post/<int:pk>/edit/", views.post_edit, name="post_edit"),
     path("tags/<int:tag_id>/", views.list_posts_by_tag, name="tag"),
 ]
